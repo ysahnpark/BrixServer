ec9c72123112220c8d7159825bbaf001440860cf
Young Suk Ahn Park <yahnpark>
2013-09-17 09:45:11 -0400

    IPS unit test checking code and message on errors

--- test/unit/sequencenodeprovider.tests.js
+++ test/unit/sequencenodeprovider.tests.js
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * $Workfile:: ams_api.tests.js                                             $
+ * $Workfile:: sequencenodeprovider.tests.js                                             $
  * *********************************************************************/ /**
  *
  * @fileoverview Contains unit tests for the sequence node retrieval interface, 
@@ -23,79 +23,7 @@ var spawn = require('child_process').spawn;
 var redis = require('redis');
 var expect = require('chai').expect;
 
-// @todo: a more "realistic" value for targetActivity field
-var targetActivityBody = {
-    "brixConfig":"...bunch of brix config goes here..."
-}
-
-/**
- * The test sequence node content.
- */
-var seqNodeBody = {
-        "guid": "course1::a8bbad4b-73e6-4713-a00c-ae9b938e1aa5::user1::http%3A%2F%2Frepo.paf.dev.pearsoncmg.com%2Fpaf-repo%2Fresources%2Factivities%2F42d2b4f4-46bd-49ee-8f06-47b4421f599b%2Fbindings%2F0",
-        "player": {
-            "guid": null,
-            "contentType": "application/vnd.pearson.qti.v2p1.asi+xml",
-            "widgetFrontend": null,
-            "toolProxy": null,
-            "frameFrontend": {
-                "guid": null,
-                "frameURI": "placeholder"
-            },
-            "preprocessor": null,
-            "postprocessor": null,
-            "@context": null,
-            "@id": null,
-            "@type": null
-        },
-        "startTime": 1376949443403,
-        "nodeIndex": 1,
-        "targetActivityXML": "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pjxhc3Nlc3NtZW50SXRlbSB4bWxucz0iaHR0cDovL3d3dy5pbXNnbG9iYWwub3JnL3hzZC9pbXNxdGlfdjJwMSIgeG1sbnM6bnMyPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hJbmNsdWRlIiB4bWxuczpuczM9Imh0dHA6Ly93d3cuaW1zZ2xvYmFsLm9yZy94c2QvaW1zbGlwX3YxcDAiIHRpdGxlPSJNQSAxLjEwLjMiIGFkYXB0aXZlPSJmYWxzZSIgdGltZURlcGVuZGVudD0iZmFsc2UiPjxyZXNwb25zZURlY2xhcmF0aW9uIGlkZW50aWZpZXI9IlJFU1BPTlNFIiBjYXJkaW5hbGl0eT0ic2luZ2xlIiBiYXNlVHlwZT0iaWRlbnRpZmllciI+PGNvcnJlY3RSZXNwb25zZT48dmFsdWU+NDwvdmFsdWU+PC9jb3JyZWN0UmVzcG9uc2U+PC9yZXNwb25zZURlY2xhcmF0aW9uPjxpdGVtQm9keT48Y2hvaWNlSW50ZXJhY3Rpb24gc2h1ZmZsZT0iZmFsc2UiIG1heENob2ljZXM9IjEiIHJlc3BvbnNlSWRlbnRpZmllcj0iUkVTUE9OU0UiPjxwcm9tcHQ+VGhlIHRleHQgb2YgJmx0O0kmZ3Q7VGhlIFNlY3JldCZsdDsvSSZndDsgaXMgcXVvdGVkIGluIHRoZSB2aWRlbyBhcyBzYXlpbmcgdGhhdCB3aGVuIHlvdSB0aGluayBvZiB0aGUgdGhpbmdzIHRoYXQgeW91IHdhbnQsIGFuZCB5b3UgZm9jdXMgb24gdGhlbSB3aXRoIGFsbCB5b3VyIGF0dGVudGlvbiwgeW91IHdpbGwgZ2V0IHdoYXQgeW91IHdhbnQsIGV2ZXJ5IHRpbWUuIFRoZSBhdXRob3IncyB0ZXJtIGZvciB0aGlzIGlkZWEgb2YgYnJpbmdpbmcgdGhpbmdzIGludG8geW91ciBsaWZlIGlzICZxdW90O19fX19fLiZxdW90OzwvcHJvbXB0PjxzaW1wbGVDaG9pY2UgaWRlbnRpZmllcj0iMSI+cHJpbmNpcGxlIG9mIHNlY3JlY3k8L3NpbXBsZUNob2ljZT48c2ltcGxlQ2hvaWNlIGlkZW50aWZpZXI9IjIiPnJ1bGUgb2YgdGhlIHVuY29uc2Npb3VzPC9zaW1wbGVDaG9pY2U+PHNpbXBsZUNob2ljZSBpZGVudGlmaWVyPSIzIj50aGVvcnkgb2YgbWluZDwvc2ltcGxlQ2hvaWNlPjxzaW1wbGVDaG9pY2UgaWRlbnRpZmllcj0iNCI+bGF3IG9mIGF0dHJhY3Rpb248L3NpbXBsZUNob2ljZT48L2Nob2ljZUludGVyYWN0aW9uPjwvaXRlbUJvZHk+PC9hc3Nlc3NtZW50SXRlbT4=",
-        "targetActivity": targetActivityBody,
-        "aggregateResult": {
-            "guid": null,
-            "attempt": null,
-            "correctOnFirstTry": null,
-            "incorrectSubmissionCount": null,
-            "numLearningAidsUsed": null,
-            "activityBinding": null,
-            "startTime": 1376949443403,
-            "duration": null,
-            "score": null,
-            "numAttempts": null,
-            "endTime": null
-        },
-        "prevNode": null,
-        "targetBinding": null,
-        "parentSequence": {
-            "guid": null,
-            "user": null,
-            "learningContext": "urn:udson:pearson.com/sms/prod:course/jsmith38271",
-            "overallActivity": "OverallActiviy, DO we need it?",
-            "@context": null,
-            "@id": "/paf-hub/resources/sequences/course1::a8bbad4b-73e6-4713-a00c-ae9b938e1aa5::user1",
-            "@type": null
-        },
-        "resultCollection": "http://hub.paf.dev.pearsoncmg.com/paf-hub/resources/sequences/course1::a8bbad4b-73e6-4713-a00c-ae9b938e1aa5::user1/nodes/1/results",
-        "endTime": null,
-        "@context": "http://purl.org/pearson/paf/v1/ctx/core/SequenceNode",
-        "@id": "http://hub.paf.dev.pearsoncmg.com/paf-hub/resources/sequences/course1::a8bbad4b-73e6-4713-a00c-ae9b938e1aa5::user1/nodes/1",
-        "@type": "SequenceNode",
-        "nodeResult": []
-    };
-
-
-/**
- * Encapsulating an assert function (i.e. chai) so it checks
- */
-function checkDoneOnException( done, fn ) {
-    try {
-        fn();
-        done();
-    } catch( e ) {
-        done( e );
-    }
-}
+var HubMock = require('./hub.mock.js');
 
 
 /**
@@ -115,7 +43,7 @@ function checkDoneOnException( done, fn ) {
 function testReqNode(seqNodeProvider, sequenceNodeIdentifier, expectError, expectData, done) {
 
     // Make sure that the cache does not have the key yet (before the proxy call)
-    redisClient = redis.createClient();
+    var redisClient = redis.createClient();
 
     seqNodeKey = seqNodeProvider.obtainSequenceNodeKey(sequenceNodeIdentifier);
 
@@ -326,16 +254,18 @@ describe('SequenceNodeProvider', function () {
     });
 
     it('should return the SequenceNode given sequence node identifier', function (done) {
-        // The nocks will intercept the HTTP call and return without requiring the actual server. 
-        setupNocks(config);
+        // The Mocks will intercept the HTTP call and return without requiring the actual server. 
+        var hubmock = new HubMock();
+        hubmock.setupNocks(config.amsBaseUrl);
         var strMessage = JSON.stringify(correctReqMessage);
-        var expectData = JSON.stringify(seqNodeBody);
+        var expectData = JSON.stringify(hubmock.seqNodeBody);
         
         testReqNode(seqNodeProvider, strMessage, null, expectData, done);
     });
 
     it('should return the SequenceNode (from cache) given sequence node key', function (done) {
-        var expectData = JSON.stringify(seqNodeBody);
+        var hubmock = new HubMock();
+        var expectData = JSON.stringify(hubmock.seqNodeBody);
 
         var seqNodeKey = seqNodeProvider.obtainSequenceNodeKey(JSON.stringify(correctReqMessage));
         seqNodeProvider.getSequenceNodeByKey(seqNodeKey, function(error, body){
@@ -404,7 +334,6 @@ describe('SequenceNodeProvider', function () {
  * body as specified in the global variable seqNodeBody
  *
  * @param {object} config  - Should contain config.amsBaseUrl.
- */
 function setupNocks(config) {
     var amsNock = nock(config.amsBaseUrl);
     amsNock.post('/seqnode')
@@ -413,6 +342,7 @@ function setupNocks(config) {
         .reply(200, JSON.stringify(seqNodeBody));
         
 }
+ */
 
 /**
  * Returns a config object with only those fields used in this test. 
