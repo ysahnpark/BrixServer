f7c0c3b6826ea036d93c683836cc3a30d1c12813
Young Suk Ahn Park <yahnpark>
2013-09-18 06:59:49 -0400

    Minor fixes to the ips unit tests

--- test/integration/ips_integration.tests.js
+++ test/integration/ips_integration.tests.js
@@ -17,7 +17,7 @@ var expect = require('chai').expect;
 var HubMock = require('../unit/hub.mock.js');
 var SequenceNodeProvider = require('../../lib/sequencenodeprovider.js');
 var request = require('supertest');  // HTTP testing  
-//var request = require('request');
+var Hapi = require('hapi');
 
 /**
  * Correctly formed interaction request message.
@@ -162,119 +162,14 @@ describe('IPC -> IPS Posting Interaction', function() {
     });
 });
 
-/*
+
 describe('IPC -> IPS Posting Submission', function() {
-    var ips = null;
-    var config = getConfig();
-    var hubmock = null;
-    var seqNodeProvider = null;
-    var seqNodeReqMessage = null;
-
-    before(function () {
-        ips = new Ips(config);
-        seqNodeProvider = new SequenceNodeProvider(config);
-
-        hubmock = new HubMock();
-        hubmock.setupNocks(hubmock.seqNodeReqMessage.url);
-
-        seqNodeReqMessage = JSON.stringify(hubmock.seqNodeReqMessage);
-        
-        // Retrieving sequence node is pre-requisite in the flow for other
-        // operations: post interaction and submission. 
-        ips.retrieveSequenceNode(seqNodeReqMessage, function(error, result) {
-            // there must be no errors
-            //console.log(result);
-            sequenceNodeKey = result.sequenceNodeKey;
-        });
-    });
-
-    it('should return a valid Node Result given correct request message', function (done) {
-        var param = cloneObject(interactionMessage);
-        // Assign the correct 
-        
-        param.sequenceNodeKey = seqNodeProvider.obtainSequenceNodeKey(seqNodeReqMessage);
-        ips.postSubmission(param, function(err, result) {
-            expect(err).to.equal(null);
-            expect(result.code).to.equal(201);
-            expect(result.data).to.be.an('object');
-            expect(result.status).to.equal('success');
-            // @todo validate the result against a schema
-            done();
-        });
-    });
-
-    it('should return error at empty sequenceNodeKey', function (done) {
-        var param = cloneObject(interactionMessage);
-        param.sequenceNodeKey = '';
-        var expectedErrorMessage = 'Invalid SequenceNodeKey';
-        ips.postSubmission(param, function(err, result) {
-            expect(err).to.equal(expectedErrorMessage);
-
-            expect(result.code).to.equal(400);
-            expect(result.message).to.equal(expectedErrorMessage);
-            expect(result.status).to.equal('error');
-            done();
-        });
-    });
     
-    it('should return error at Empty Request Body', function (done) {
-        var param = cloneObject(interactionMessage);
-        param.sequenceNodeKey = sequenceNodeKey;
-        param.body = '';
-        var expectedErrorMessage = 'Invalid body';
-        ips.postSubmission(param, function(err, result) {
-            expect(err).to.equal(expectedErrorMessage);
-
-            expect(result.code).to.equal(400);
-            expect(result.message).to.equal(expectedErrorMessage);
-            expect(result.status).to.equal('error');
-            done();
-        });
-    });
-
-    it('should return error at SequenceNodeKey not found', function (done) {
-        var param = cloneObject(interactionMessage);
-        param.sequenceNodeKey = 'ABC';
-        var expectedErrorMessage = 'SequenceNodeKey not found';
-        ips.postSubmission(param, function(err, result) {
-            expect(err).to.equal(expectedErrorMessage);
-
-            expect(result.code).to.equal(404);
-            expect(result.message).to.equal(expectedErrorMessage);
-            expect(result.status).to.equal('error');
-            done();
-        });
-    });
-
-    it('should return error at Hub-Session expired', function (done) {
-        // How can we explicitly expire hub session?
-        var param = cloneObject(interactionMessage);
-        param.sequenceNodeKey = 'ABC';
-        var expectedErrorMessage = 'Hub-Session expired';
-        ips.postSubmission(param, function(err, result) {
-            expect(err).to.equal('Hub-Session expired');
-
-            expect(result.code).to.equal(400);
-            expect(result.message).to.equal(expectedErrorMessage);
-            expect(result.status).to.equal('error');
-            done();
-        });
-    });
 });
 
 describe('IPC -> IPS retrieveSequenceNode Test', function () {
-    var ips = null;
-    var config = getConfig();
-
-    before(function () {
-        var ips = new Ips(config);
-    });
-
-    it('returns the Sanitized SequenceNode', function (done) {
-        done();
-    });
+    
 });
-*/
 
 /**
  * Returns a config object with only those fields used in this test. 
