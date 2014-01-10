ae7ae8d579969cd9de68e5e9de99fbe4b9974e2b
Young Suk Ahn Park <youngsuk.ahnpark@pearson.com>
2014-01-10 11:21:43 -0500

    added client lib location setting on configSchma

--- configSchema.js
+++ configSchema.js
@@ -23,6 +23,9 @@ module.exports = {
     "logDir":               Joi.types.String().optional(),
     "logAllowWebAccess":    Joi.types.Boolean().optional(),
 
+    "brixClientBuildAccess":Joi.Types.Boolean().optional(),
+    "brixClientBuildDir":   Joi.Types.String().required(),
+
     "imgDir":               Joi.Types.String().required(),
     "imgBaseUrl":           Joi.Types.String().required()
 };
